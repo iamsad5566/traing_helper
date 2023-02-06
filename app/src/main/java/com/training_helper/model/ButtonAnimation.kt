6fd4163b8858a81166e0aca580c931040f58d5c8
Yen-Kuang-tw <nf8964p5566@gmail.com>
2023-02-07 00:00:16 +0800

    feat: change touch to click

--- app/src/main/java/com/training_helper/model/ButtonAnimation.kt
+++ app/src/main/java/com/training_helper/model/ButtonAnimation.kt
@@ -1,40 +1,23 @@
 package com.training_helper.model
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.os.CountDownTimer
-import android.util.Log
-import android.view.MotionEvent
 import android.view.View
 
 class ButtonAnimation() {
-
-    @SuppressLint("ClickableViewAccessibility")
     fun buttonEffect(button: View, timeGear: View, setGear: View) {
-        button.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-
-                    val timer = object : CountDownTimer(300, 10) {
-                        override fun onTick(millisUntilFinished: Long) {
-                            v.scaleX += 0.01f
-                            v.scaleY += 0.01f
-                        }
-
-                        override fun onFinish() {
-                        }
-                    }
-                    timer.start()
-
-                    v.invalidate()
+        button.setOnClickListener {
+            val timer = object : CountDownTimer(300, 10) {
+                override fun onTick(millisUntilFinished: Long) {
+                    it.scaleX += 0.01f
+                    it.scaleY += 0.01f
                 }
-                MotionEvent.ACTION_UP -> {
-                    v.scaleY = 1f
-                    v.scaleX = 1f
-                    v.invalidate()
+
+                override fun onFinish() {
+                    it.scaleY = 1f
+                    it.scaleX = 1f
                 }
             }
-            false
+            timer.start()
         }
     }
 }
