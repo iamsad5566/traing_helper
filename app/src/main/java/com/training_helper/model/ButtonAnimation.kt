e48493fdd5fe9b1908a6ce40195e442d8e8584b6
Yen-Kuang-tw <nf8964p5566@gmail.com>
2023-02-07 00:41:13 +0800

    feat: add animation for the gears

--- app/src/main/java/com/training_helper/model/ButtonAnimation.kt
+++ app/src/main/java/com/training_helper/model/ButtonAnimation.kt
@@ -2,10 +2,36 @@ package com.training_helper.model
 
 import android.os.CountDownTimer
 import android.view.View
+import com.training_helper.R
 
 class ButtonAnimation() {
+    var turning: Boolean = false
+
+
     fun buttonEffect(button: View, timeGear: View, setGear: View) {
+        var timeGearTurning = object : CountDownTimer(60000, 10) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeGear.rotation += 0.3f
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+
+        var setGearTurning = object : CountDownTimer(60000, 10) {
+            override fun onTick(millisUntilFinished: Long) {
+                setGear.rotation -= 0.45f
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+
+
         button.setOnClickListener {
+            turning = !turning
             val timer = object : CountDownTimer(300, 10) {
                 override fun onTick(millisUntilFinished: Long) {
                     it.scaleX += 0.01f
@@ -18,6 +44,16 @@ class ButtonAnimation() {
                 }
             }
             timer.start()
+
+            if(turning) {
+                it.setBackgroundResource(R.drawable.pause)
+                timeGearTurning.start()
+                setGearTurning.start()
+            } else {
+                it.setBackgroundResource(R.drawable.play)
+                timeGearTurning.cancel()
+                setGearTurning.cancel()
+            }
         }
     }
 }
